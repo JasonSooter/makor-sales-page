37a125014677b5c1d6bd3bbf4aa05f46d9826940
JasonSooter <jason.sooter@gmail.com>
2018-05-22 12:53:27 -0500

    Added Google Analytics Code

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,16 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-119664067-1',
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/']
+      }
+    }
   ]
 };
